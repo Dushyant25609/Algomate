5db7d741da179183e8abb3dd10a9e9c6cba5004c
Dushyant <117421735+Honey25609@users.noreply.github.com>
2025-04-07 15:27:47 +0530

    bug fix

--- src/components/ui/chart/Pie-chart.tsx
+++ src/components/ui/chart/Pie-chart.tsx
@@ -45,40 +45,41 @@ export interface PieChartProps {
   className?: string;
 }
 
-function Legend({ data }: { data: ChartData[] }) {
+function Legend({ data = [] }: { data: ChartData[] }) {
   const [hover, setHover] = React.useState<number>(-1);
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
       <div className="flex md:flex-col gap-2 md:gap-5">
-        {data.map((item, index) => (
-          <LegendItem className="w-16 md:w-20" key={index}>
-            <Typography
-              onMouseEnter={() => setHover(index)}
-              onMouseLeave={() => setHover(-1)}
-              borderColor={item.color}
-              color={item.color}
-              fontWeight={hover === index ? 'bold' : 'normal'}
-              className={cn(
-                'border w-full  rounded-sm transition-colors duration-500 md:px-3 py-2 flex items-center justify-center',
-                item.label === 'Easy' ? 'bg-[#08d1b9]/20 hover:bg-[#08d1b9]/40' : '', // Replace with a valid color
-                item.label === 'Medium' ? 'bg-[#FDBF1E]/20 hover:bg-[#FDBF1E]/40' : '',
-                item.label === 'Hard' ? 'bg-[#FF375F]/20 hover:bg-[#FF375F]/40' : '',
-                item.label === 'CP' ? 'bg-[#C05AF2]/20 hover:bg-[#C05AF2]/40' : '',
-                item.label === 'Leetcode' ? 'bg-[#FFA217]/20 hover:bg-[#FFA217]/40' : ''
-              )}
-              variant="body2"
-            >
-              {hover === index ? item.value : item.label}
-            </Typography>
-          </LegendItem>
-        ))}
+        {data &&
+          data.map((item, index) => (
+            <LegendItem className="w-16 md:w-20" key={index}>
+              <Typography
+                onMouseEnter={() => setHover(index)}
+                onMouseLeave={() => setHover(-1)}
+                borderColor={item.color}
+                color={item.color}
+                fontWeight={hover === index ? 'bold' : 'normal'}
+                className={cn(
+                  'border w-full  rounded-sm transition-colors duration-500 md:px-3 py-2 flex items-center justify-center',
+                  item.label === 'Easy' ? 'bg-[#08d1b9]/20 hover:bg-[#08d1b9]/40' : '', // Replace with a valid color
+                  item.label === 'Medium' ? 'bg-[#FDBF1E]/20 hover:bg-[#FDBF1E]/40' : '',
+                  item.label === 'Hard' ? 'bg-[#FF375F]/20 hover:bg-[#FF375F]/40' : '',
+                  item.label === 'CP' ? 'bg-[#C05AF2]/20 hover:bg-[#C05AF2]/40' : '',
+                  item.label === 'Leetcode' ? 'bg-[#FFA217]/20 hover:bg-[#FFA217]/40' : ''
+                )}
+                variant="body2"
+              >
+                {hover === index ? item.value : item.label}
+              </Typography>
+            </LegendItem>
+          ))}
       </div>
     </Box>
   );
 }
 
 export default function PieChartWithCenterLabel({
-  ChartValues,
+  ChartValues = [],
   COLORS,
   height,
   width,
@@ -86,7 +87,10 @@ export default function PieChartWithCenterLabel({
   outerRadius,
   className,
 }: PieChartProps) {
-  const totalQuestions = ChartValues.reduce((sum, item) => sum + item.value, 0);
+  const totalQuestions =
+    ChartValues && ChartValues.length > 0
+      ? ChartValues.reduce((sum, item) => sum + item.value, 0)
+      : 0;
   const size = {
     width: width,
     height: height,
@@ -104,7 +108,7 @@ export default function PieChartWithCenterLabel({
           colors={COLORS}
           series={[
             {
-              data: ChartValues,
+              data: ChartValues || [],
               innerRadius: innerRadius,
               outerRadius: outerRadius,
               highlightScope: { fade: 'global', highlight: 'item' },
