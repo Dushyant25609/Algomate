04d9c5fde5d76664a513af7fc39155d91062d74f
Dushyant <117421735+Honey25609@users.noreply.github.com>
2025-04-18 17:41:02 +0530

    new

--- src/components/ui/chart/Pie-chart.tsx
+++ src/components/ui/chart/Pie-chart.tsx
@@ -116,7 +116,7 @@ export default function PieChartWithCenterLabel({
               innerRadius: innerRadius,
               outerRadius: outerRadius,
               highlightScope: { fade: 'global', highlight: 'item' },
-              faded: { innerRadius: 55, additionalRadius: -10, color: 'gray' },
+              faded: { innerRadius: 55, additionalRadius: -5, color: 'gray' },
             },
           ]}
           {...size}
