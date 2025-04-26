a325662398a48b05e086636ddff4f8646e91bc44
Dushyant <117421735+Honey25609@users.noreply.github.com>
2025-04-26 09:17:09 +0530

    removed sheets

--- src/constants/nav-items.ts
+++ src/constants/nav-items.ts
@@ -4,5 +4,5 @@ import { AppRoutes } from '@/lib/routes';
 export const defaultNavItems: NavItem[] = [
   { label: 'Home', href: AppRoutes.HOME },
   { label: 'Leaderboard', href: AppRoutes.LEADERBOARD },
-  { label: 'Sheets', href: AppRoutes.SHEETS },
+  // { label: 'Sheets', href: AppRoutes.SHEETS },
 ];
